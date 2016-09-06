8fa923233ed1f1440a3ae2250cdfc49e4b051a32
endy <admin@site15.ru>
2016-09-06 22:04:20 +0500

    add npm modules

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,11 +3,19 @@ sudo apt-get autoremove nodejs -y
 curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
 sudo apt-get install -y nodejs
 sudo apt-get install npm -y
-sudo npm update -g
+sudo npm update -g    
 sudo npm install -g npm
-sudo pip install --upgrade pip
+sudo npm install -g git+https://git@github.com/gulpjs/gulp.git#4.0
+sudo npm install -g karma-cli
+sudo npm install -g npm-check-updates
+sudo npm install -g bower
+sudo npm install -g protractor
+sudo npm install -g selenium-webdriver
+sudo npm install -g tslint
+sudo npm install -g typescript
 sudo npm install -g node-gyp
 sudo npm rebuild
+sudo pip install --upgrade pip
 sudo apt-get update
 pip install -r requirements.txt
 cd frontend
